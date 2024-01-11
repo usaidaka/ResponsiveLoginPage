a99d1c98d89a88f4c197a63f12c83884c63f0201
usaid AKA <ujedkemal@gmail.com>
2024-01-11 16:25:34 +0700

    responsive login page

--- script.js
+++ script.js
@@ -2,11 +2,15 @@
 function myFunction() {
   let x = document.getElementById("myInput");
   let y = document.getElementById("off");
+  let z = document.getElementById("on");
+
   if (x.type === "password") {
     x.type = "text";
     y.style.display = "block";
+    z.style.display = "none";
   } else {
     x.type = "password";
     y.style.display = "none";
+    z.style.display = "block";
   }
 }
